461fda23086693eee9ed77d86ccdda7463e1db57
araxielfenix <araxielfenix@outlook.com>
2022-11-25 21:06:56 -0600

    Se ajustó la lectura de los archivos y animacion.

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 // On load page, hide the "result" table.
 window.onload = function () {
     document.getElementById("result").style.display = "none";
-    document.getElementById("loader").style.display = "none";
+    document.getElementById("loadingAnimation").style.display = "none";
+    document.getElementById("descargarButton").style.display = "none";
 }
 
-var lista = [];
-var file1 = [];
-var file2 = [];
-var fileSplit1 = [];
-var fileSplit2 = [];
+var archivo = [];
+var archivo2 = [];
 
 function getFileName1() {
-    var fileInput = document.getElementById('file-upload1');
-    var filename = fileInput.files[0].name;
+    let fileInput = document.getElementById('file-upload1');
+    let filename = fileInput.files[0].name;
     document.getElementById("fileLabel1").innerHTML = filename;
-    file1 = document.getElementById('file-upload1').files[0];
+    let file1 = document.getElementById('file-upload1').files[0];
     if (file1) {
-        var reader = new FileReader();
+        let reader = new FileReader();
         reader.readAsText(file1, "UTF-8");
         reader.onload = function (evt) {
-            fileSplit1 = evt.target.result.split("\t");
+            let fileSplit1 = evt.target.result.split("\t");
             fileSplit1 = fileSplit1.toString().split("\r");
             fileSplit1 = fileSplit1.toString().split("\n");
+            fileSplit1 = fileSplit1.toString().split(",");
+            fileSplit1 = fileSplit1.filter(function (el) {
+                return el != "";
+            });
+            let fileSplit1Length = fileSplit1.length;
+            for(let i = 0; i < fileSplit1Length; i++){
+                archivo[i] = [];
+                for(let j = 0; j < 12; j++){
+                    archivo[i][j] = fileSplit1[i+j];
+                }
+                i += 11;
+            }
         }
         reader.onerror = function (evt) {
             alert("No se pudo leer el archivo Log");
@@ -30,17 +40,30 @@ function getFileName1() {
 }
 
 function getFileName2() {
-    var fileInput = document.getElementById('file-upload2');
-    var filename = fileInput.files[0].name;
+    let fileInput = document.getElementById('file-upload2');
+    let filename = fileInput.files[0].name;
     document.getElementById("fileLabel2").innerHTML = filename;
-    file2 = document.getElementById('file-upload2').files[0];
+    let file2 = document.getElementById('file-upload2').files[0];
     if (file2) {
-        var reader = new FileReader();
+        let reader = new FileReader();
         reader.readAsText(file2, "UTF-8");
         reader.onload = function (evt2) {
-            fileSplit2 = evt2.target.result.split("\t");
+            let fileSplit2 = evt2.target.result.split("\t");
             fileSplit2 = fileSplit2.toString().split("\r");
             fileSplit2 = fileSplit2.toString().split("\n");
+            fileSplit2 = fileSplit2.toString().split(",");
+            //.pop every 13 elements.
+            fileSplit2 = fileSplit2.filter(function (el) {
+                return el != "";
+            });
+            let fileSplit2Length = fileSplit2.length;
+            for(let i = 0; i < fileSplit2Length; i++){
+                archivo2[i] = [];
+                for(let j = 0; j < 5; j++){
+                    archivo2[i][j] = fileSplit2[i+j];
+                }
+                i += 4;
+            }
         }
         reader.onerror = function (evt2) {
             alert("No se pudo leer el archivo complemento");
@@ -48,113 +71,81 @@ function getFileName2() {
     }
 }
 
-function comparacion() {
-    //Wait 1 second before starting the comparison.
-    setTimeout(function () {
-        if (fileSplit1 && fileSplit2) {
-            var arlog = logFile(fileSplit1);
-            var ardiff = difAtt(fileSplit2);
-            var comp = [];
-            //If "arlog" starts with a letter, do nothing.
-            if (arlog[0][0].match(/[a-z]/i)) {
-                alert("Asegurate de haber ingresado el archivo correcto o de haber eliminado los encabezados");
-            }
-            else {
-                //compare the two arrays and return the values that match.
-                arlog.map((row, index) => {
-                    for (var i = 0; i < ardiff.length; i++) {
-                        if (row[3] == ardiff[i][0].substring(9, ardiff[i][0].length)) {
-                            comp[index] = [row[0], row[4], row[11].substring(8, 17), row[6], row[5], row[7], row[3], ardiff[i][2]];
-                        }
+async function loading(){
+    document.body.style.cursor = "wait";
+    document.getElementById("loadingAnimation").style.display = "inline-block";
+    document.getElementById("compararButton").innerHTML = "Comparando...";
+    //Espera 5 segundos antes de ejecutar la funcion comparacion para que se vea el progreso de la barra.
+    await new Promise(r => setTimeout(r, 1000));
+    await comparacion();
+    console.log("Comparación completada");
+    document.getElementById("compararButton").innerHTML = "Validando...";
+    await new Promise(r => setTimeout(r, 1000));
+    await addData();
+    document.getElementById("compararButton").innerHTML = "Comparar";
+    document.getElementById("loadingAnimation").style.display = "none";
+    document.body.style.cursor = "default";
+}
+var comp = [];
+async function comparacion() {
+    return new Promise(async (resolve, reject) => {
+        if (archivo && archivo2) {
+            console.log("Comparando...");
+            archivo2.map((filas, index) => {
+                archivo.map((filas2, index2) => {
+                    if(archivo2[index][0].includes(archivo[index2][3]) && archivo2[index][3].includes(archivo[index2][4]) &&  archivo2[index][4].includes(archivo[index2][6])){
+                        comp[index] = [archivo[index2][0], archivo[index2][4], archivo[index2][5], archivo[index2][6], archivo[index2][8], archivo[index2][10], archivo[index2][9], archivo2[index][2]];
                     }
                 });
-                //remove empty or undefined values from the array.
-                var comp2 = comp.filter(function (el) {
-                    return el != null;
-                });
-                //remove the duplicates from the array.
-                var comp3 = comp2.filter(function (item, pos) {
-                    return comp2.indexOf(item) == pos;
-                });
-                //add the data to the table.
-                comp3.map((row, index) => {
-                    addData(row);
-                });
-            }
+            } );
         }
         else {
             alert("No se puede completar la comparación, revise los archivos");
-            document.body.style.cursor = "default";
         }
-    }, 1000);
-    document.getElementById("loader").style.display = "block";
-
-}
-
-function duplicados(){
-    //from the "Lista" array, find the duplicates and return them.
-    var duplicados = [];
-    for (var i = 0; i < lista.length; i++) {
-        if (lista.indexOf(lista[i]) != lista.lastIndexOf(lista[i])) {
-            duplicados.push(lista[i]);
-        }
-    }
-    //remove the duplicates from the array.
-    var duplicados2 = duplicados.filter(function (item, pos) {
-        return duplicados.indexOf(item) == pos;
+        resolve();
     });
-    //Turn the array into a csv file.
-    var csvContent = "data:text/csv;charset=utf-8,";
-    duplicados2.forEach(function (rowArray) {
-        var row = rowArray.join(",");
-        csvContent += row + "\r";
+}
+
+function duplicados() {
+
+}
+
+async function addData() {
+    return new Promise((resolve, reject) => {
+        console.log("Agregando datos a la tabla...");
+        let tableRow = document.getElementById("result");
+        let row = tableRow.insertRow(1);
+        let cells = [];
+        let compLenght = comp.length;
+
+        comp.map((filas, index) => {
+            cells[index] = [];
+            for(let i = 0; i < 8; i++){
+                cells[index] = row.insertCell(i);
+                cells[index].innerHTML = comp[index][i];
+            }
+        });
+
+        document.getElementById("result").style.display = "inline-table";
+        document.getElementById("descargarButton").style.display = "inline-block";
+        resolve();
     });
-    //Download the file.
-    var encodedUri = encodeURI(csvContent);
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "duplicados.csv");
-    document.body.appendChild(link);
-    link.click();
-}
-
-function addData(data) {
-    lista = data;
-    var tableRow = document.getElementById("result");
-    var row = tableRow.insertRow(1);
-    var cells = [];
-    data.map((rows, index) => {
-        cells[index] = row.insertCell(index);
-        cells[index].innerHTML = data[index];
-    });
-    document.getElementById("result").style.display = "inline-table";
-    document.getElementById("loader").style.display = "none";
-}
-
-function logFile(log) {
-    return log.map(row => row.split(","));
-}
-
-function difAtt(diff) {
-    return diff.map(row => row.split(","));
 }
 
 function downloadList() {
     //Download the table as a CSV file using the "download" attribute.
-    var csv = 'data:text/csv;charset=utf-8,';
-    var rows = document.querySelectorAll("table tr");
-    for (var i = 0; i < rows.length; i++) {
-        var row = [], cols = rows[i].querySelectorAll("td, th");
-        for (var j = 0; j < cols.length; j++)
+    let csv = 'data:text/csv;charset=utf-8,';
+    let rows = document.querySelectorAll("table tr");
+    for (let i = 0; i < rows.length; i++) {
+        let row = [], cols = rows[i].querySelectorAll("td, th");
+        for (let j = 0; j < cols.length; j++)
             row.push(cols[j].innerText);
         csv += row.join(",") + "\r";
     }
-    var encodedUri = encodeURI(csv);
-    var link = document.createElement("a");
+    let encodedUri = encodeURI(csv);
+    let link = document.createElement("a");
     link.setAttribute("href", encodedUri);
     link.setAttribute("download", "archivoMatch.csv");
     document.body.appendChild(link);
     link.click();
-
-    duplicados();
 }
